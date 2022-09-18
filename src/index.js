71966fe1aba2b4443e6cafa26d75bd2e934377be
NaingNaingHtun <naingnainghtun984@gmail.com>
2022-09-18 10:39:30 +0630

    replaced BrowserRouter with HashRouter

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login/Login";
 import UserList from "./components/UserList/UserList";
 import User from "./pages/User/User";
